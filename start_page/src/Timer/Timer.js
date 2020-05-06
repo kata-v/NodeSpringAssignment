5ffa502f7070b32d5ee6087c66979f55cbdf4dd6
KataV <KataV@users.noreply.github.com>
2020-05-06 21:41:09 +0300

    Built the main structure of the quiz start page with components for timer, user banner, how to play

---
+++ start_page/src/Timer/Timer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Timer/Timer.css';
+
+const Timer = () => {
+    return (
+        <div className="timerContainer">
+            <p>0:00</p>
+        </div>
+    );
+}
+
+export default Timer;
